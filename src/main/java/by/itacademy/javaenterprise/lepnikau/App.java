3cf9973b65cbbeae4c44d386ea65000a1f0a7d76
ViachaslauL <Lotussutol@tut.by>
2021-11-18 20:00:32 +0300

    refactor: App.java

--- src/main/java/by/itacademy/javaenterprise/lepnikau/App.java
+++ src/main/java/by/itacademy/javaenterprise/lepnikau/App.java
@@ -3,12 +3,15 @@ package by.itacademy.javaenterprise.lepnikau;
 import by.itacademy.javaenterprise.lepnikau.dao.AddressDAO;
 import by.itacademy.javaenterprise.lepnikau.dao.impl.AddressDAOImpl;
 import by.itacademy.javaenterprise.lepnikau.entity.Address;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class App {
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
     public static void main(String[] args) {
 
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("by.it");
@@ -16,6 +19,6 @@ public class App {
         AddressDAO addressDAO = new AddressDAOImpl(entityManager);
 
         Address address = addressDAO.find(1L);
-        System.out.println(address);
+        LOG.info(address.toString());
     }
 }
